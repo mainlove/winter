3ec7a217123bcc0c7fb577ccdbf6d7aba8691a5e
mainlove <wmainlove@gmail.com>
2016-11-29 14:39:43 +0800

    init

--- src/main/java/io/forlove/framework/winter_springboot/App.java
+++ src/main/java/io/forlove/framework/winter_springboot/App.java
@@ -1,10 +1,29 @@
 package io.forlove.framework.winter_springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import java.util.Stack;
+
 /**
  * Created by eju on 2016/11/17.
  */
-public class App {
+@SpringBootApplication
+public class App extends SpringBootServletInitializer
+        implements EmbeddedServletContainerCustomizer
+    {
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(10086);
     }
 }

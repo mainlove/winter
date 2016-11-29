29a660c88ac98906d80127d61ab56e2d18d0d41c
mainlove <wmainlove@gmail.com>
2016-11-29 18:36:28 +0800

    init

--- src/main/java/io/forlove/framework/winter_springboot/App.java
+++ src/main/java/io/forlove/framework/winter_springboot/App.java
@@ -17,7 +17,6 @@ public class App extends SpringBootServletInitializer
         implements EmbeddedServletContainerCustomizer
     {
 
-
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
--- src/main/java/io/forlove/framework/winter_springboot/MvcConfigurer.java
+++ src/main/java/io/forlove/framework/winter_springboot/MvcConfigurer.java
@@ -2,22 +2,13 @@ package io.forlove.framework.winter_springboot;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class MvcConfigurer extends WebMvcConfigurerAdapter {
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/error").setViewName("error.html");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    }
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        super.configurePathMatch(configurer);
-        configurer.setUseSuffixPatternMatch(false);
     }
 }
